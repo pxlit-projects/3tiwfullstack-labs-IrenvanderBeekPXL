3122d999dc5f3138be3b369322832aa49441d187
IrenvanderBeekPXL <Iren.vanderBeek@student.pxl.be>
2024-10-02 11:55:46 +0200

    Added base app and Customer.ts

---
+++ frontend-web/customer-app/src/Customer.ts
@@ -0,0 +1,11 @@
+export class Customer {
+  name: string;
+  email: string;
+  city: string;
+  address: string;
+  country: string;
+  vat: number;
+  isLoyal: boolean;
+  avatar: string;
+  id?: number;
+}
